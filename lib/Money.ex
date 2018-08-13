fa03f665ede7645cd0100341a3a70c58c7b8ad80
Lucas Rolim <lucasrolim@poli.ufrj.br>
2018-08-13 20:44:03 -0300

    basic money operations

---
+++ lib/Money.ex
@@ -0,0 +1,3 @@
+defmodule Finance.Money do
+  defstruct currency: :nil, amount: nil, exchange_rate: nil, precision: nil
+end
--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule StoneChallenge.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, "~> 0.12"}
+      {:ex_doc, "~> 0.12"},
+      {:decimal, "~> 1.0"}
     ]
   end
 end
